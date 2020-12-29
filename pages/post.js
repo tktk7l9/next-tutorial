bd60a4aaab9d6fb2fcb217874ebba112d1cf9c19
tktk7l9 <saitotakuya0719@gmail.com>
2020-12-29 19:38:50 +0900

    コンポーネントをスタイリングする

--- pages/post.js
+++ pages/post.js
@@ -1,11 +1,35 @@
 import Layout from '../components/MyLayout.js'
 import fetch from 'isomorphic-unfetch'
+import Markdown from 'react-markdown'
 
 const Post = (props) => (
     <Layout>
         <h1>{props.show.name}</h1>
-        <p>{props.show.summary.replace(/<[/]?p>/g, '')}</p>
-        <img src={props.show.image.medium}/>
+        <div className="markdown">
+            <p>{props.show.summary.replace(/<[/]?p>/g, '')}</p>
+        </div>
+        <img src={props.show.image.medium} />
+        <style jsx global>{`
+            .markdown {
+                font-family: 'Arial';
+            }
+
+            .markdown a {
+                text-decoration: noen;
+                color: blue;
+            }
+
+            .markdown a:hover {
+                opacity: 0.6;
+            }
+
+            .markdown h3 {
+                margin: 0;
+                padding: 0;
+                text-transform: uppercase;
+            }
+        `}
+        </style>
     </Layout>
 )
 
